fa6860a3cc99bf3f0b45faae22a52decb7fce7e5
Juhee Han <81957899+okodevhub@users.noreply.github.com>
2021-12-03 18:07:06 +0900

    feature : 지역정보 call api
    상위지역과 특정상위지역의 하위지역을 불러올 수 있는 api를 작성

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from pymongo.collation import Collation
 
 app = Flask(__name__)
 
+client = MongoClient("localhost", 27017)
+db = client.dbspartatoyprojcet
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
+
+
+# 지역정보전체를 가져오는 API
+@app.route("/regionRoots", methods=["GET"])
+def get_all_reionRoots():
+    regionRoots = list(
+        db.regions.find({}, {"_id": False})
+        .sort("regionRootCode", 1)
+        .collation(Collation(locale="en_US", numericOrdering=True))
+    )
+    return jsonify({"regionRoots": regionRoots})
+
+
+# 특정 상위지역과 그 하위지역을 가져오는 API
+@app.route("/regionRoots/<regionRootCode>", methods=["GET"])
+def get_regionRoot(regionRootCode):
+    region_root = db.regions.find_one(
+        {"regionRootCode": str(regionRootCode)},
+        {"_id": False},
+        sort=[("regionSub.regionSubCode", 1)],
+    )
+
+    return jsonify({"regionRoot": region_root})
 
 
 @app.route("/test", methods=["GET"])
